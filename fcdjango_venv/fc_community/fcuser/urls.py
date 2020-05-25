dbe9d5e1c950f06b39da2f4e0c1b7c020012952d
seunghakbae <onelife0717@gmail.com>
2020-05-25 23:24:51 +0900

    updated django

---
+++ fcdjango_venv/fc_community/fcuser/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+# admin안에 여러 url이 정의되어 있다. 
+# aaaa.com/admin/~~~/~~~ <= 장고의 admin을 관리.
+
+urlpatterns = [
+    path('register/', views.register),
+    path('login/', views.login),
+]
---
+++ fcdjango_venv/fc_community/fcuser/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+from .models import Fcuser
+from django.http import HttpResponse
+
+def home(request):
+    user_id = request.session.get('user')
+
+    if user_id:
+        fcuser = Fcuser.objects.get(pk=user_id)
+        return HttpResponse(fcuser.username)
+    
+    return HttpResponse('Home!')
+
+# Create your views here.
+def login(request):
+
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not (username and password):
+            res_data['error'] = '모든 값을 입력해야합니다.'
+        else:
+            fcuser = Fcuser.objects.get(username=username)
+            if check_password(password, fcuser.password):
+                
+                # 로그인 처리 비밀번호 일치
+                # 세션
+                # Home으로 redirect하는 코드도 작성
+
+                request.session['user'] = fcuser.id
+                return redirect('/')
+            else:
+                res_data['error'] = '비밀번호를 틀렸습니다.'
+
+        return render(request, 'login.html', res_data)
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+        
+        res_data = {}
+
+        if not (username and password and re_password and useremail):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:            
+            
+            fcuser = Fcuser(
+                username = username,
+                password = make_password(password),
+                useremail = useremail
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
